612bf97c5a8e3700fd30f509e1789fd8f9ef3fd1
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-24 21:01:15 +0300

    Added webpack scripts to the js folder

---
+++ js/webpack-demo/src/color.js
@@ -0,0 +1,22 @@
+const rndColor = Math.floor(Math.random() * 255);
+console.log(rndColor);
+
+const colrwrapper = document.createElement("div");
+
+const wrpper = document.querySelector(".color-wrapper");
+
+document.querySelector(".btn-color").addEventListener("click", () => {
+  wrpper.appendChild(colrwrapper);
+
+  colrwrapper.style.backgroundColor = generateColor();
+});
+
+const generateColor = () => {
+  var optn = "0123456789ABCDEF";
+  var color = "#";
+
+  for (var i = 0; i < 6; i++) {
+    color += optn[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
