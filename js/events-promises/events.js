915d69d0d98ef6bdebf3a32c239f1ca460242903
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-26 15:54:40 +0300

    Added myproject folder in the js

--- js/events-promises/events.js
+++ js/events-promises/events.js
@@ -1,72 +1,3 @@
-// JS EVENTS
-//built in events
-const addN_ame = (e) => {
-  console.log(e);
-  const name = e.target.value;
-  document.querySelector(".name-list").innerHTML = `My name is ${name}`;
-};
-//const btnName = document.querySelector(".btn-name");
-
-//btnName.addEventListener("click", addName);
-//btnName.removeEventListener("click", addName);
-
-//implementation
-const addFName = (name) => {
-  console.log(name);
-  document.querySelector(".name-wrapper").innerHTML = `My name is ${name}`;
-};
-const btnAddName = document.querySelector(".add-name");
-
-btnAddName.addEventListener("click", (e) => {
-  const name = document.querySelector(".name").value;
-  addFName(name);
-});
-
-//CUSTOM EVENTs
-//using the event constructor
-const myEvvent = new Event("myEvent", {
-  bubbles: true,
-  cancelable: true,
-  composed: true,
-});
-
-//implementation
-const btnClick1 = new Event("btnClick", {
-  bubbles: true,
-  composed: true,
-  cancelable: true,
-});
-// btnName.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   addName();
-// });
-
-//btnName.dispatchEvent(btnClick);
-
-//using the custom event constructor
-const myEvent = new CustomEvent("myEvent", {
-  bubbles: true,
-  cancelable: true,
-  composed: true,
-  detail: {},
-});
-
-//implementation
-const colorEvent = new CustomEvent("color", {
-  bubbles: true,
-  composed: true,
-  cancelable: true,
-});
-
-document.querySelector(".btn-hover").addEventListener("mouseover", (e) => {
-  e.preventDefault();
-  const random = Math.floor(Math.random() * 255);
-  document.querySelector(
-    ".event-js"
-  ).style.backgroundColor = `rgb(${random}, ${random}, ${random})`;
-});
-document.querySelector(".btn-hover").dispatchEvent(colorEvent);
-
 //DYNAMIC TYPING
 var num = 13;
 var wrd = "Stirng";
@@ -95,52 +26,54 @@ promise
 
 //impelementation
 
-document.querySelector(".btn-confirm").addEventListener("click", () => {
-  let pswd = document.querySelector(".pwd").value;
-  let confirmPwd = document.querySelector(".confirm-pwd").value;
-  let matchPassword = new Promise((resolve, reject) => {
-    if (pswd === confirmPwd) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
+let pswd = document.querySelector(".pwd").value;
+let confirmPwd = document.querySelector(".confirm-pwd").value;
 
-  matchPassword
-    .then(() => {
-      document.querySelector(".error-mesge").innerHTML =
-        "Passwords are a match!!";
-    })
-    .catch(() => {
-      document.querySelector(".error-mesge").style.color = "red";
-      document.querySelector(".error-mesge").innerHTML =
-        "Passwords do not match!!";
-    });
+let matchPassword = new Promise((resolve, reject) => {
+  if (pswd === confirmPwd) {
+    setTimeout(() => {
+      resolve("passwords");
+    }, 2000);
+  } else {
+    reject();
+  }
 });
 
-const addName = () => {
-  const name = prompt("Enter task name");
-  document.querySelector(".name-list").innerHTML = name;
+const message = async () => {
+  const resp = await matchPassword;
+  console.log(resp);
 };
-const changeBgcolor = () => {
-  document.querySelector(".name-list").style.backgroundColor =
-    "rgba(255,255,5,0.8)";
-};
-// const btnName = document.querySelector(".btn-name");
 
-// btnName.addEventListener("click", addName);
-// btnName.addEventListener("click", changeBgcolor);
+let isLogin = true;
 
-const btnClick = new Event("btnClick", {
-  bubbles: true,
-  composed: true,
-  cancelable: true,
-  detail: {},
+const login = new Promise((resolve, reject) => {
+  if (isLogin) {
+    setTimeout(() => {
+      console.log("Logging in");
+      resolve(isLogin);
+    }, 1000);
+  } else {
+    reject();
+  }
 });
-// btnName.addEventListener("click", (event) => {
-//   console.log(event.detail);
-//   event.preventDefault();
-//   addName();
-// });
 
-// btnName.dispatchEvent(btnClick);
+const seeLogin = async () => {
+  const response = await login;
+  console.log(response);
+};
+
+const [messge, slogin] = await promise.all(message, seeLogin);
+const btnconfirm = document.querySelector(".btn-confirm");
+btnconfirm.addEventListener("click", () => {
+  messge;
+  slogin;
+});
+
+const seeLogin1 = async () => {
+  try {
+    const resp = await login;
+    console.log(resp);
+  } catch (error) {
+    console.log(error);
+  }
+};
